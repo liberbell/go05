b440b0828c4186b3d3b2ffa8590356333da7dccd
Liberbell <liberbell@gmail.com>
2020-01-07 07:57:40 +0900

    mean3

--- mean.go
+++ mean.go
@@ -11,4 +11,8 @@ func main() {
 
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
+
+	var mean int
+	mean = (x + y) / 2
+	fmt.Printf("resutl: %v, type of %T\n", mean, mean)
 }
