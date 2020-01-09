4c2496f3991b2425c2d97812d260bce776e7da9b
Liberbell <liberbell@gmail.com>
2020-01-09 14:56:36 +0900

    switch2

---
+++ switch.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 2
+
+	switch x {
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	case 3:
+		fmt.Println("Three")
+	default:
+		fmt.Println("Many")
+	}
+}
