b117cea0edd6676673aea5b7c9022a8148c26589
Liberbell <liberbell@gmail.com>
2020-01-09 15:03:04 +0900

    switch4

--- switch.go
+++ switch.go
@@ -15,4 +15,13 @@ func main() {
 	default:
 		fmt.Println("Many")
 	}
+
+	switch {
+	case x > 100:
+		fmt.Println("x is very big")
+	case x > 10:
+		fmt.Println("x is big")
+	default:
+		fmt.Println("x is small")
+	}
 }
