deb1a8e1e62d0f1229b343fe12531c91ec906321
Liberbell <liberbell@gmail.com>
2020-01-11 16:24:34 +0900

    fizzbuzz3

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
 	for i := 1; i <= 20; i++ {
 		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("Fizz Buzzs")
+			fmt.Println(i, "Fizz Buzzs")
+		} else if i%3 == 0 {
+			fmt.Println(i, "Fizz")
 		}
 	}
 }
