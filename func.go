d0d1c0684581440758bd434dfbb9da4f89c36cdc
Liberbell <liberbell@gmail.com>
2020-01-15 07:47:21 +0900

    func2

---
+++ func.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func add(a int, b int) (int, int) {
+	return a + b
+}
+
+func divmod(a int, b int) (int, int) {
+	return a / b, a % b
+}
+
+func main() {
+	val := add(1, 2)
+	fmt.Println(val)
+}
