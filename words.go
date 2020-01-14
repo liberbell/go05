7d75c2d19bf4b92a02adb290808a9e6410628d27
Liberbell <liberbell@gmail.com>
2020-01-15 07:38:57 +0900

    words1

--- words.go
+++ words.go
@@ -1,11 +1,23 @@
-pacakge main
+package main
+
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-  text := `
+	text := `
 	Needles and pins
 	Needles and pins
 	Sew me a sail
 	To catch me the wind
 	`
-  
+
+	words := strings.Fields(text)
+	counts := map[string]int{}
+
+	for _, word := range words {
+		counts[strings.ToLower(word)]++
+	}
+	fmt.Println(counts)
 }
