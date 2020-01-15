a3f5bc9002b7ee33f3a6428d72e97aa0299428ed
Liberbell <liberbell@gmail.com>
2020-01-16 07:34:57 +0900

    params1

--- params.go
+++ params.go
@@ -6,8 +6,20 @@ func doubleAt(values []int, i int) {
 	values[i] *= 2
 }
 
+func dubule(n int) {
+	n *= 2
+}
+
+func doublePtr(n *int) {
+	*n *= 2
+}
+
 func main() {
 	values := []int{1, 2, 3, 4}
 	doubleAt(values, 0)
 	fmt.Println(values)
+
+	val := 10
+	double(10)
+	fmt.Println(val)
 }
