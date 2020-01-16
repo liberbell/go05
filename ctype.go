2ef6b67710dbf1aab2743c557a054183f7c33af6
Liberbell <liberbell@gmail.com>
2020-01-17 07:53:36 +0900

    ctype6

--- ctype.go
+++ ctype.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func contentType(url string) (string, err) {
+func contentType(url string) (string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return "", err
