18e7c245681853b17b7b483326353480c27e56dc
Liberbell <liberbell@gmail.com>
2020-01-18 15:46:54 +0900

    trade1

--- trade.go
+++ trade.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
 type Trade struct {
 	Symbol string
 	Volume int
 	Price  float64
 	Buy    bool
+}
+
+func main() {
+	t1 := Trade{"MSFT", 10, 99.98.true}
+	fmt.Println(t1)
 }
