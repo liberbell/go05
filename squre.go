2ea47bae639fe2b9b569bbf9caea03cea9b558af
Liberbell <liberbell@gmail.com>
2020-01-21 07:49:16 +0900

    squre3

--- squre.go
+++ squre.go
@@ -4,3 +4,9 @@ type Squre struct {
 	Center Point
 	Length int
 }
+
+func NewSqure(x int, y int, length int) (*Squre error) {
+  if length <= 0 {
+    return nil, fmt.Errorf("length must be > 0 (was %d)\n", length)
+  }
+}
