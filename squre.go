2eef0e49fd64f7cae57f74e113d80da5db1a5c8c
Liberbell <liberbell@gmail.com>
2020-01-21 07:50:05 +0900

    squre4

--- squre.go
+++ squre.go
@@ -9,4 +9,9 @@ func NewSqure(x int, y int, length int) (*Squre error) {
   if length <= 0 {
     return nil, fmt.Errorf("length must be > 0 (was %d)\n", length)
   }
+
+  s := &Squre{
+    Center: Point{x, y},
+    Length: length,
+  }
 }
