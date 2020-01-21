4c08c4532c1e57274d337dc47b9135f86d5fbb6c
Liberbell <liberbell@gmail.com>
2020-01-22 08:01:00 +0900

    shapes4

--- shapes.go
+++ shapes.go
@@ -1,17 +1,19 @@
 package main
 
+import "math"
+
 type Square struct {
-  Length float64
+	Length float64
 }
 
 func (s, *Square) Area() float64 {
-  return s.Length * s.Length
+	return s.Length * s.Length
 }
 
-type Circle strcut {
-  Radius float64
+type Circle struct {
+	Radius float64
 }
 
 func (c, *Circle) Area() float64 {
-  return math.Pi * c.Radius * c.Radius
+	return math.Pi * c.Radius * c.Radius
 }
