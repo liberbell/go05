e9f244355ab0db4cedfae7f61b43af7872cc9846
Liberbell <liberbell@gmail.com>
2020-01-24 06:41:10 +0900

    errors2-1

--- errors2.go
+++ errors2.go
@@ -1,5 +1,14 @@
 package main
 
+import "os"
+
 type Config struct {
 	//
+}
+
+func readConfig(path string) (*Config, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
 }
