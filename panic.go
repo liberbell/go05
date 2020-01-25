280acb8425ee9e997da77726eef68206c9d2cc91
Liberbell <liberbell@gmail.com>
2020-01-25 10:55:10 +0900

    panic9

--- panic.go
+++ panic.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func safeValue(vals []int, index int) int {
@@ -18,10 +17,13 @@ func main() {
 	// vals := []int{1, 2, 3}
 	// v := vals[10]
 	// fmt.Println(v)
-	file, err := os.Open("no-such.file")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	fmt.Println("file opened")
+	// file, err := os.Open("no-such.file")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer file.Close()
+	// fmt.Println("file opened")
+
+	v := safeValue([]int{1, 2, 3}, 10)
+	fmt.Printf(v)
 }
