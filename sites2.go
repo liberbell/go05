6f2aaf78fdb8be6bdc0a8425d688a7f14f1bc942
Liberbell <liberbell@gmail.com>
2020-01-28 11:16:22 +0900

    sites2-8

--- sites2.go
+++ sites2.go
@@ -9,30 +9,30 @@ func returnType(url string, out chan string) {
 	resp, err := http.Get(url)
 	if err != nil {
 		out <- fmt.Sprintf("%s - error: %s", url, err)
-    return
+		return
 	}
 
-  defer resp.Body.Close()
-  ctype := resp.Header.Get("content-type")
-  out <- fmt.Sprintf("%s -> %s", url ctype)
+	defer resp.Body.Close()
+	ctype := resp.Header.Get("content-type")
+	out <- fmt.Sprintf("%s -> %s", url, ctype)
 }
 
 func main() {
-  urls := []string{
-    "https://golang.org",
-    "https://api.github.com",
-    "https://httpbin.org/xml",
-    "https://yahoo.co.jp",
-    "https://www.ntt-at.co.jp",
-  }
+	urls := []string{
+		"https://golang.org",
+		"https://api.github.com",
+		"https://httpbin.org/xml",
+		"https://yahoo.co.jp",
+		"https://www.ntt-at.co.jp",
+	}
 
-  ch := make(chan string)
-  for _, url := range url {
-    go returnType(url, ch)
-  }
+	ch := make(chan string)
+	for _, url := range url {
+		go returnType(url, ch)
+	}
 
-  for range urls {
-    out := <-ch
-    fmt.Println(out)
-  }
+	for range urls {
+		out := <-ch
+		fmt.Println(out)
+	}
 }
