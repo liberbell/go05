0b76e5e0cf104264f694e3ce979ff5dda691898b
Liberbell <liberbell@gmail.com>
2020-01-28 11:12:38 +0900

    sites2-3

--- sites2.go
+++ sites2.go
@@ -9,5 +9,10 @@ func returnType(url string, out chan string) {
 	resp, err := http.Get(url)
 	if err != nil {
 		out <- fmt.Sprintf("%s - error: %s", url, err)
+    return
 	}
+
+  defer resp.Body.Close()
+  ctype := resp.Header.Get("content-type")
+  out <- fmt.Sprintf("%s -> %s", url ctype)
 }
