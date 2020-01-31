42b548dc110967786d49bc51ea3ed14beba85e14
Liberbell <liberbell@gmail.com>
2020-01-31 09:57:42 +0900

    cfg2

--- cfg.go
+++ cfg.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+
+	toml "github.com/pelletier/go-toml"
 )
 
 type Config struct {
@@ -16,5 +18,12 @@ func main() {
 	file, err := os.Open("config.toml")
 	if err != nil {
 		log.Fatalf("error: can`t open config file - %s\n", err)
+	}
+	defer file.Close()
+
+	cfg := &Config{}
+	dec := toml.NewDecoder(file)
+	if err := dec.Decode(cfg); err != nil {
+		log.Fatalf("error: can`t decode configuration file - %s\n", err)
 	}
 }
