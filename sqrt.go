d6084115bc0659ca352d80da2114fedfe64099be
Liberbell <liberbell@gmail.com>
2020-02-01 11:26:21 +0900

    sqrt2

--- sqrt.go
+++ sqrt.go
@@ -1,5 +1,12 @@
 package main
 
+import "errors"
+
+var (
+	ErrNegSqrt    = errors.New("sqrt of negative number")
+	ErrNoSolution = errors.New("no solution found")
+)
+
 func Abs(val float64) float64 {
 	if val < 0 {
 		return -val
