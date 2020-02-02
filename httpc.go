6fe42918a4613bfaf14d668a1e09465754b3f3ba
Liberbell <liberbell@gmail.com>
2020-02-03 08:01:40 +0900

    httpc3

--- httpc.go
+++ httpc.go
@@ -1,7 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
 	resp, err := http.Get("https://httpbin.org/get")
+	if err != nil {
+		log.Fatalf("Error: can`t call httpbin.org")
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
 }
