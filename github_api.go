a19ae9fa346b1c40d92ce64107ad5d0be4cc8fc9
Liberbell <liberbell@gmail.com>
2020-02-05 07:31:34 +0900

    github_api1

--- github_api.go
+++ github_api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -17,4 +18,7 @@ func userInfo(login string) (*User, error) {
 		return nil, err
 	}
 	defer resp.Body.Close()
+
+	user := &User{}
+	dec := json.NewDecoder(resp.Body)
 }
