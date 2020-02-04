8583ca2acdae421b776d801448b46328a5a4ebda
Liberbell <liberbell@gmail.com>
2020-02-05 07:53:59 +0900

    httpd3

--- httpd.go
+++ httpd.go
@@ -10,6 +10,12 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello! Gophers.")
 }
 
+type MathRequest struct {
+	Op    string  `json:"op"`
+	Left  float64 `json:"left"`
+	Right float64 `json:"right"`
+}
+
 func main() {
 	http.HandleFunc("/hello", helloHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
