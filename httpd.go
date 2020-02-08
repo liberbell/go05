ba881cba7663ef0ed29314e68d9d8fa12f34e3ee
Liberbell <liberbell@gmail.com>
2020-02-08 23:11:05 +0900

    httpd7

--- httpd.go
+++ httpd.go
@@ -47,6 +47,8 @@ func kvPostHandler(w http.ResponseWriter, r *http.Request) {
   dbLock.Lock()
   defer dbLock.Unlock()
   db[entry.Key] = entry.Value
+
+  sendResponse(entry, w)
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
